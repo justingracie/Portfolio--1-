44ebd5bc8bc5d09d166a01b94c33f6ca9df4a8bb
justingracie <j.m.gracie@gmail.com>
2022-08-18 15:51:01 -0600

    added icons to navbar

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -4,12 +4,12 @@ import { NavLink } from "react-router-dom";
 export const NavBar = () => {
     return(
         <>
-            <NavLink to='/'> Home | </NavLink>     
-            <NavLink to='/contact'> Contact |  </NavLink>     
-            <NavLink to='/about'> About |  </NavLink>     
-            <NavLink to='/services'> Services |  </NavLink>     
-            <NavLink to='/projects'> Projects |  </NavLink>     
-            <NavLink to='/skills'> Skills |  </NavLink>     
+            <NavLink to='/'> <img src="../Icons/icons8-home-48.png" alt="home"/> </NavLink>     
+            <NavLink to='/contact'> <img src="../Icons/icons8-contact-50.png" alt="contact"/></NavLink>     
+            <NavLink to='/about'><img src="../Icons/icons8-about-50.png" alt="about"/> </NavLink>     
+            <NavLink to='/services'> <img src="../Icons/icons8-services-58.png" alt="services"/></NavLink>     
+            <NavLink to='/projects'> <img src="../Icons/icons8-group-of-projects-50.png" alt="projects"/>  </NavLink>     
+            <NavLink to='/skills'> <img src="../Icons/icons8-skills-60.png" alt="skills"/></NavLink>     
         </>
     )
 }
