aa52b078d521035d19430485c4ec8b6455f2b122
justingracie <j.m.gracie@gmail.com>
2022-08-18 16:14:51 -0600

    navbar V1 complete

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import './Nav.css'
 
 export const NavBar = () => {
     return(
         <>
-            <NavLink to='/'> <img src="../Icons/icons8-home-48.png" alt="home"/> </NavLink>     
-            <NavLink to='/contact'> <img src="../Icons/icons8-contact-50.png" alt="contact"/></NavLink>     
-            <NavLink to='/about'><img src="../Icons/icons8-about-50.png" alt="about"/> </NavLink>     
-            <NavLink to='/services'> <img src="../Icons/icons8-services-58.png" alt="services"/></NavLink>     
-            <NavLink to='/projects'> <img src="../Icons/icons8-group-of-projects-50.png" alt="projects"/>  </NavLink>     
-            <NavLink to='/skills'> <img src="../Icons/icons8-skills-60.png" alt="skills"/></NavLink>     
+            <div className="NavContainer">
+            
+                <NavLink className="NavItem" to='/'> <img src="../Icons/icons8-home-48.png" alt="home"/> </NavLink>     
+                <NavLink className="NavItem" to='/contact'> <img src="../Icons/icons8-contact-50.png" alt="contact"/></NavLink>     
+                <NavLink className="NavItem" to='/about'><img src="../Icons/icons8-about-50.png" alt="about"/> </NavLink>     
+                <NavLink className="NavItem" to='/services'> <img src="../Icons/icons8-services-58.png" alt="services"/></NavLink>     
+                <NavLink className="NavItem" to='/projects'> <img src="../Icons/icons8-group-of-projects-50.png" alt="projects"/>  </NavLink>     
+                <NavLink className="NavItem" to='/skills'> <img src="../Icons/icons8-skills-60.png" alt="skills"/></NavLink>     
+                <a className="NavItem" href="https://www.linkedin.com/in/justin-gracie-6ab58a67/"><img src="../Icons/icons8-linkedin-circled-50.png" alt="Linkedin"/></a>
+                <a className="NavItem" href="https://github.com/justingracie"><img src="../Icons/icons8-github-48.png" alt="Linkedin"/></a>
+
+            </div>
         </>
     )
 }
