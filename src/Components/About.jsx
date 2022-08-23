880a90974dbcf529077b2a3b01798b0daea29c7e
justingracie <j.m.gracie@gmail.com>
2022-08-23 15:47:24 -0600

    added placeholder about page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,7 +1,20 @@
 export const About = () =>{
     return(
         <>
-            <h1>About page goes here...</h1>
+            <div className="aboutContainer">
+                <img className="image" src="./Images/Cartoon_JG.png" alt="cartoon_me"></img>
+                <div>
+                
+                    <p className="aboutCopy">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+                    molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+                    numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
+                    optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+                    obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+                    nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+                    tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+                    quia.</p>            
+                </div>
+            </div>
         </>
     )
 }
