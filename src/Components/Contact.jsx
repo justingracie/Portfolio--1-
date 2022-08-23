0cacb7fb0ffc8c51e7d3067a9bfe115a0c333d9d
justingracie <j.m.gracie@gmail.com>
2022-08-23 10:11:29 -0600

    fixed form position

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -25,24 +25,28 @@ export const ContactUs = () => {
 
     return(
         
-        <div className="formContainer">
-            <div className="form-box">
-                <h5>Contact Me</h5>
-                <form ref={form} onSubmit={sendEmail}>
-                    <div className="form">
-                        <label>Name:</label>
-                            <input type="text" name="user_name" placeholder="NAME"/>
-                        <label>Email:</label>
-                            <input type="email" name="user_email" placeholder="EMAIL"/>
-                        <label>Message:</label>
-                            <textarea name="message" placeholder="MESSAGE"/>
-                            <input type="submit" value="Send" className="sendBtn"/>
-                            </div>
-                            </form>
-                            </div>
-                            <ReCAPTCHA
-                                sitekey="6LejhZwhAAAAAFDpFYGvj_ACX5Z5pHamJG5wHFds"
-                                onChange={onChange}/>
-        </div>
+
+        
+            <div className="formContainer">
+                <div className="form-box">
+                    <h5>Contact Me</h5>
+                    <form ref={form} onSubmit={sendEmail}>
+                        <div className="form">
+                            <label>Name:</label>
+                                <input type="text" name="user_name" placeholder="NAME"/>
+                            <label>Email:</label>
+                                <input type="email" name="user_email" placeholder="EMAIL"/>
+                            <label>Message:</label>
+                                <textarea name="message" placeholder="MESSAGE"/>
+                                <input type="submit" value="Send" className="sendBtn"/>
+                                </div>
+                                </form>
+                                </div>
+                                <ReCAPTCHA
+                                    sitekey="6LejhZwhAAAAAFDpFYGvj_ACX5Z5pHamJG5wHFds"
+                                    onChange={onChange}
+                                    className="recap"/>
+            </div>
+
     );
 };   
