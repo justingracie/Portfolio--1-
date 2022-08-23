11ad891777ccce0fb8a8de9e96f8ee27ce8a71b7
justingracie <j.m.gracie@gmail.com>
2022-08-23 11:23:19 -0600

    added skills and basic styling

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,7 +1,44 @@
 export const Skills = () => {
     return(
         <>
-            <h1>Skills list goes here...</h1>
+        <div className="titleContainer">
+            <h1 className="title">Skills!</h1>
+        </div>
+        <div className="skillContainer">
+                <div className="frontSkill">
+                    <h1>Front-end:</h1>
+                    <ul>
+                        <li>HTML</li>
+                        <li>JavaScript</li>
+                        <li>CSS</li>
+                        <li>Bootstrap</li>
+                        <li>React</li>
+                    </ul>
+                </div>
+                <div className="backSkill">
+                    <h1>Back-end:</h1>
+                    <ul>
+                        <li>Node JS</li>
+                        <li>MongoDB</li>
+                        <li>Postgres</li>
+                        <li>Django</li>
+                        <li>Python</li>
+                    </ul>
+                
+                </div>
+                <div className="backSkill">
+                    <h1>Additional:</h1>
+                    <ul>
+                        <li>Github</li>
+                        <li>Miro</li>
+                        <li>Slack</li>
+                        <li>VSCode</li>
+                        <li>Heroku</li>
+                    </ul>
+                
+                </div>
+            </div>    
+
         </>
     )
 }
