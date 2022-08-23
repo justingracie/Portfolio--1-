ee293b2fe41a9f48cb5d6b864307f6ba4f9fe352
justingracie <j.m.gracie@gmail.com>
2022-08-23 09:10:33 -0600

    tweaked nav and links

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -13,8 +13,8 @@ export const NavBar = () => {
                 <NavLink className="NavItem" to='/services'> <img src="../Icons/icons8-services-58.png" alt="services"/></NavLink>     
                 <NavLink className="NavItem" to='/projects'> <img src="../Icons/icons8-group-of-projects-50.png" alt="projects"/>  </NavLink>     
                 <NavLink className="NavItem" to='/skills'> <img src="../Icons/icons8-skills-60.png" alt="skills"/></NavLink>     
-                <a className="NavItem" href="https://www.linkedin.com/in/justin-gracie-6ab58a67/"><img src="../Icons/icons8-linkedin-circled-50.png" alt="Linkedin"/></a>
-                <a className="NavItem" href="https://github.com/justingracie"><img src="../Icons/icons8-github-48.png" alt="Linkedin"/></a>
+                <a className="NavItem" href="https://www.linkedin.com/in/justin-gracie-6ab58a67/" target="_blank" rel="noreferrer"><img src="../Icons/icons8-linkedin-circled-50.png" alt="Linkedin"/></a>
+                <a className="NavItem" href="https://github.com/justingracie" target="_blank" rel="noreferrer"><img src="../Icons/icons8-github-48.png" alt="Linkedin"/></a>
 
             </div>
         </>
